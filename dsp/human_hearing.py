f38d3434c2990483ec550c02531a5d4b7fbb7b8d
Karoska <kar.piwowarczyk@gmail.com>
2021-01-13 18:27:55 +0100

    implementacja słyszenia pod woda

--- dsp/human_hearing.py
+++ dsp/human_hearing.py
@@ -1,14 +1,14 @@
 from numpy import cos, sin, pi, absolute, arange
-from scipy.signal import kaiserord, lfilter, firwin, freqz
+from scipy.signal import kaiser_beta, lfilter, firwin, freqz
 
-def lpf(sample_rate,x,cutoff_hz=500.0, width = 250.0,ripple_db = 25.0):
+def lpf(sample_rate,x,cutoff_hz=500.0, attenuation = 25):
 
   nyq_rate = sample_rate / 2.0
-  width = width/nyq_rate
-  ripple_db = ripple_db
-  N, beta = kaiserord(ripple_db, width)
+  attenuation=attenuation
+  N=100
+  beta = kaiser_beta(attenuation)
   cutoff_hz = cutoff_hz
   taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
-  filtered_x = lfilter(taps, 1.0, x)
+  filtered_x = lfilter(taps, [1.0,0], x)
 
   return filtered_x
