a87b4821e686f6f1dc5985e68425c13d713cd1c5
piter <piotrjciarka@gmail.com>
2021-01-17 18:14:31 +0100

    impulse response in wav file

--- dsp/revebration_one.py
+++ dsp/revebration_one.py
@@ -1,5 +1,10 @@
 
-def get_rir(audio_signal, fs, rt60=0.2, room_dim =[60, 60, 10], room_source=[30, 30, 4.5], mic_pos=[30, 10, 7], T=19, D=0.01, S=35):
+def get_rir(fs, rt60=0.2,
+            room_dim=[60, 60, 10],
+            room_source=[30, 30, 4.5],
+            mic_pos=[30, 10, 7],
+            T=19, D=0.01, S=35,
+            save_to_wav=False):
     import pyroomacoustics as pra
     import numpy as np
     c = 1449.2+4.6*T-0.055*T**2+0.0029*T**3+(1.34-0.01*T)*(S-35)+0.016*D
@@ -7,7 +12,7 @@ def get_rir(audio_signal, fs, rt60=0.2, room_dim =[60, 60, 10], room_source=[30,
     room = pra.ShoeBox(
         room_dim, fs=fs, materials=pra.Material(e_absorption),ray_tracing=False, max_order=3 ,air_absorption=False
     )
-    room.add_source(room_source, signal=audio_signal, delay=1.0)
+    room.add_source(room_source, delay=1.0)
     mic_locs = np.c_[
         mic_pos,  # mic 1
     ]
